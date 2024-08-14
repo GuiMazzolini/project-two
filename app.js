fe8b71f54d63a52d4698a1a27b52a60c2137f4ab
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2024-08-14 17:42:27 +0200

    bug fixed

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
-// ℹ️ Connects to the database
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
 const path = require("path");
 const app = express();
@@ -18,10 +11,7 @@ const app = express();
 
 app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "views/partials"));
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
-
-
 
 const capitalize = require("./utils/capitalize");
 const projectName = "project-two";
@@ -31,21 +21,19 @@ app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 const session = require("express-session")
 const MongoStore = require("connect-mongo")
 
-  // ℹ️ Middleware that adds a "req.session" information and later to check that you are who you say you are 😅
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || "super hyper secret key",
+      secret: "super hyper secret key",
       resave: true,
       saveUninitialized: true,
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
+        mongoUrl: "mongodb://localhost:27017/iron-library"
       }),
     })
   );
 
 const projectRoutes = require("./routes/project.routes");
 app.use("/", projectRoutes);
-// 👇 Start handling routes here
 
 const indexRoutes = require("./routes/index.routes");
 app.use("/", indexRoutes);
@@ -65,9 +53,6 @@ app.use("/", profileRoutes);
 const dataRoutes = require("./routes/data.routes");
 app.use("/", dataRoutes);
 
-
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
